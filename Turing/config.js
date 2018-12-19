173c9b65992d309a12eeae7cf084a4a42419253c
riceCk <876496731@qq.com>
2018-12-19 20:29:06 +0800

    手机自适应处理处理2

--- Turing/config.js
+++ Turing/config.js
@@ -6,8 +6,9 @@ let conf = fs.readFileSync('./server.conf')
 let globalConfig = {}
 let configArr = conf.toString().split('\r\n')
 for (let i = 0; i < configArr.length; i++) {
-	let key = configArr[i].split('=')[0]
+	let key = configArr[i].split('=')[0].trim()
 	let value = configArr[i].split('=')[1]
 	globalConfig[key] = value
 }
+console.log(globalConfig)
 module.exports = globalConfig
